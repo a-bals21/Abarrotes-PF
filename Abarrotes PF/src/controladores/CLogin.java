04f09424b9dfda801fc5cfa00132037524498174
Luis <A20216349@alumnos.uady.mx>
2023-05-07 01:48:41 -0600

    no hice nada x2

--- Abarrotes PF/src/controladores/CLogin.java
+++ Abarrotes PF/src/controladores/CLogin.java
@@ -8,10 +8,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 
 /**
